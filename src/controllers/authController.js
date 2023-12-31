7c532120f44fce206794c9577e2779ee1d1f38d9
ruancs <ruan.cs@hotmail.com>
2023-12-31 02:47:21 -0300

    feat:tratativa de erro-controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,9 +3,12 @@ import authService from "../services/authService.js";
 async function signup(req , res){
     const body = req.body;
 
-    const resService = await authService.signup(body)
-    
-    res.send(resService)
+    try{
+        const resService = await authService.signup(body);
+        res.status(201).send(resService)
+    }catch (err){
+        res.status(409).send(err.message)
+    }
 }
 
 export default {signup}
